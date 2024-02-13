9392f0e1591d0eea3aa2dbf48d64b51a2fa844da
anthonynjenga254 <anthonynjenga2020@gmail.com>
2024-02-13 16:00:48 +0300

    today, I worked on a web form using vue, I learnt how to use v-model directive, two-way data binding, listening to events, etc

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+ <SignupForm />
+</template>
+
+
+<script>
+import SignupForm from './components/icons/SignupForm.vue'
+export default {
+  name: 'App' ,
+  components: { SignupForm }
+}
+</script>
+
+
+<style>
+#app {
+  font-family: Avenir, Arial, Helvetica, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body {
+  margin: 0;
+  background: #eee;
+}
+
+
+</style>
